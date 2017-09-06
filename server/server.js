191614f4389e99e3b536de23d9e82c15dedbec3d
Frankenfield <Kozyrov>
2017-09-05 20:41:45 -0700

    Built findall and findById GET routes. Used ObjectID from native methods to simpify processing the ID.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 //npm/libaray imports
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 //local imports
 const {mongoose} = require('./db/mongoose');
@@ -32,6 +33,33 @@ app.get('/order', (req, res)=>{
         res.status(400).send(err);
     })
 })
+
+app.get('/order/:orderID', (req, res)=>{
+    let orderID = req.params.orderID;
+    if(!ObjectID.isValid(orderID)){
+        res.status(404).send();
+    } else {
+        Order.findById(orderID).then((order)=>{
+            if(!order){
+                res.status(404).send();
+            } else {
+                res.send({order});
+            }
+        }).catch((err)=>{
+            res.status(400).send();
+        })
+    }
+});
+
+//isValid
+    //404 - call send without a value.
+
+//find by id
+    //success
+        //if todo send it back
+        //if no todo - if no todo - send back a 404 with nothing send with no argument
+    //error
+        //400 - send back nothing
 
 app.listen(3001, ()=>{
     console.log('Started on port 3001');
